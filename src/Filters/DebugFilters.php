8209ac255d53ac710578c6e5a730bfc24623e232
Fabio Capucci <fabio.cappuc@gmail.com>
2023-09-22 15:22:02 +0200

    fix dump filter

--- src/Filters/DebugFilters.php
+++ src/Filters/DebugFilters.php
@@ -8,9 +8,12 @@ class DebugFilters extends FiltersProvider
 {
     public function dump(mixed $value): mixed
     {
+        ob_start();
         dump($value);
+        $dump = ob_get_contents();
+        ob_end_clean();
 
-        return $value;
+        return $dump;
     }
 
     public function dd(mixed $value): void
