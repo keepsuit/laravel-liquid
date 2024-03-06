47f01240af4dbe8a063e6e4cb4c01eaf124c738f
Fabio Capucci <fabio.cappuc@gmail.com>
2024-03-06 11:15:49 +0100

    fixed types

--- src/Filters/DebugFilters.php
+++ src/Filters/DebugFilters.php
@@ -6,17 +6,21 @@ use Keepsuit\Liquid\Filters\FiltersProvider;
 
 class DebugFilters extends FiltersProvider
 {
-    public function dump(mixed $value): mixed
+    public function dump(mixed $value): string
     {
         ob_start();
         dump($value);
         $dump = ob_get_contents();
         ob_end_clean();
 
+        if ($dump === false) {
+            return '';
+        }
+
         return $dump;
     }
 
-    public function dd(mixed $value): void
+    public function dd(mixed $value): never
     {
         dd($value);
     }
